ac6456d638210eca6376a22ca46449eff575505d
Francisco Javier Santiago Muñoz <frangarmu95@gmail.com>
2022-05-04 21:27:01 -0500

    Llamado correcto en index.php

--- index.php
+++ index.php
@@ -13,6 +13,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // echo json_encode($route->getRoutes());
-$app = new App\Core\Core($route->getRoutes());
+$app = new Core\Core($route->getRoutes());
 $app->send();
 
